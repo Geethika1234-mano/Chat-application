37720757196c4bbb499dfae88c01ed82e0d54413
Tharindu Rasmal <130370267+tharindurasmal@users.noreply.github.com>
2024-06-29 08:58:20 +0530

    routerLink and routerLinkActive

--- client/src/app/Pages/sign-in/sign-in.component.ts
+++ client/src/app/Pages/sign-in/sign-in.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
--- client/src/app/Pages/sign-up/sign-up.component.ts
+++ client/src/app/Pages/sign-up/sign-up.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
